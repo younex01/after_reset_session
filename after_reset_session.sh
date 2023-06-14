8f348e8be9ae85ecb9030f1c0d67c0ad67215464
Youness Elousse <yelousse@e1r4p10.1337.ma>
2023-06-14 18:36:37 +0100

    feat: install ohmyzsh and add code cmd to path

---
+++ after_reset_session.sh
@@ -0,0 +1,3 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \ 
+echo export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin" >> ~/.zshrc && \
+source ~/.zshrc
